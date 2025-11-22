225ee1a8021cd4530b2b793a16ff2e2cffc3c47f
sanmathibs <sanmathi@blueskycreations.com.au>
2025-11-22 15:37:57 +1100

    testing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from tabs import (
 
 
 st.set_page_config(
-    page_title="Mecca Sim – Demand forecasting & labour scheduling",
+    page_title="Mecca Simulator - with live forecasting",
     page_icon="🚀",
     layout="wide",
 )
